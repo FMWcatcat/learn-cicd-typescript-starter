5627341f3caa5609f21a0fab382713cdac451050
FMWcatcat <nikomakela15@gmail.com>
2025-08-10 05:50:13 +0300

    test

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,4 @@
-import { IncomingHttpHeaders } from "http";
+/*import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
@@ -12,4 +12,9 @@ export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   }
 
   return splitAuth[1];
+}
+*/
+
+export function getAPIKey(headers: IncomingHttpHeaders): string | null {
+  return "definitely-wrong";
 }
